cc745200680b07376cc6aab4aa4056f4dcb65e08
fleal <freddy.alejandro.leal@gmail.com>
2021-12-17 07:41:33 -0500

    Ejercicio del useContext

--- 05-hook-app/src/components/09-useContext/LoginScreen.js
+++ 05-hook-app/src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
 
 export const LoginScreen = () => {
+
+    const {user, setUser} = useContext(UserContext);
+
     return (
         <div>
             <h1>LogingScreen</h1>
             <hr />
+
+            <pre>
+                { JSON.stringify(user, null, 3)}
+            </pre>
+
+            <button 
+                className='btn btn-primary'
+                onClick={ () => {
+                    setUser({
+                        id: '1234',
+                        name: 'Freddy Leal',
+                        email: 'freddy.alejandro.leal@gmail.com'
+                    })
+                }}
+            >Login</button>
         </div>
     )
 }
--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const [user, setUser] = useState({})
+
     return (
-        <div>
-            <h1>MainApp</h1>
-            <hr />
-        </div>
+        <UserContext.Provider value={{
+            user,
+            setUser,
+        }} >
+
+            <AppRouter />
+            
+        </UserContext.Provider>
     )
 }
